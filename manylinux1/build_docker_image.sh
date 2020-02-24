5347cc70794d66da7ed98e7a837bbf59d48039f4
Jason Patton <jpatton@cs.wisc.edu>
2020-02-24 14:15:48 -0600

    Fix tag and push

--- manylinux1/build_docker_image.sh
+++ manylinux1/build_docker_image.sh
@@ -3,7 +3,8 @@ set -ex
 
 DOCKERFILE_DIR=$(dirname "$(readlink -f "$0")")/docker/
 DOCKER_REPO=dockerreg.chtc.wisc.edu
-TAG=$(head -n 1 latest_tag)
+TAG="htcondor_manylinux1_x86_64:$(head -n 1 latest_tag)"
 
-docker build $DOCKERFILE_DIR -t htcondor_manylinux1_x86_64:$TAG
-docker push $DOCKER_REPO/htcondor/htcondor_manylinux1:$TAG
+docker build $DOCKERFILE_DIR -t $TAG
+docker tag $TAG $DOCKER_REPO/htcondor/$TAG
+docker push $DOCKER_REPO/htcondor/$TAG
