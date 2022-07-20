d766f8568ae4a18c9efab4bb2e8119c96e6f9b1f
Tim Theisen <tim@cs.wisc.edu>
2022-07-20 10:30:55 -0500

    HTCONDOR-1087: Fix typo in tagging

--- manylinux2014/build_docker_image.sh
+++ manylinux2014/build_docker_image.sh
@@ -5,5 +5,5 @@ DOCKERFILE_DIR=$(dirname "$(readlink -f "$0")")/docker/
 TAG="htcondor_manylinux2014_x86_64:$(head -n 1 latest_tag)"
 
 docker build $DOCKERFILE_DIR -t $TAG
-docker tag htcondor/$TAG
+docker tag $TAG htcondor/$TAG
 docker push htcondor/$TAG
