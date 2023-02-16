6e69bd3ed8ceebd29fcaa8d9fb2d0e2668b033f4
Tim Theisen <tim@cs.wisc.edu>
2023-02-16 17:03:45 -0600

    HTCONDOR-1586: Always build our Docker image on the latest release

--- manylinux2014/build_docker_image.sh
+++ manylinux2014/build_docker_image.sh
@@ -4,6 +4,7 @@ set -ex
 DOCKERFILE_DIR=$(dirname "$(readlink -f "$0")")/docker/
 TAG="htcondor_manylinux2014_x86_64:$(head -n 1 latest_tag)"
 
+docker pull quay.io/pypa/manylinux2014_x86_64
 docker build $DOCKERFILE_DIR -t $TAG
 docker tag $TAG htcondor/$TAG
 docker push htcondor/$TAG
